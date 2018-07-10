d8da6ffa193ecdabe9b250a8456cefce741a37b1
ChristopheGueroult <christophegueroult@gmail.com>
2018-07-10 16:52:48 +0200

    architecture de l'appli et ng bootstrap

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CoreModule } from './core/core.module';
+import { ItemsModule } from './items/items.module';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    PageNotFoundModule,
+    ItemsModule,
+    NgbModule.forRoot(),
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
